391e72b29754ed0b85f11e4e8f5db0d386d18644
Mounir el bertouli <58659995+Mounir24@users.noreply.github.com>
2021-03-03 21:17:20 +0100

    new updates

--- public/main.js
+++ public/main.js
@@ -24,19 +24,20 @@ $(document).ready(function() {
         socket.emit('typing', $('#user').val());
     })
 
+     // Broadcast message to the sockets
+     socket.on('typing', (payload) => {
+        $('#feedback').text(payload + ' typing...');
+    })
+
     // When a message sent to server , append it to DOM
     socket.on('chat', (payload) => {
         $('#feedback').text('');
         //$('#messages').html(`<strong>${payload.username}</strong>: ${payload.msg} (${payload.date})`);
-        $('#messages').append($('<p>').text(payload.user + ': ' + payload.message));
+        $('#messages').append($('<p>').text(payload.username + ': ' + payload.msg + ' (' + payload.date + ')'));
     })
 
     // When a user connected to server , he w'll displayed in DOM
-    socket.emit('disconnect', (socket) => {
+    socket.emit('disconnect', () => {
             alert('You disconnect');
         })
-        // Broadcast message to the sockets
-    socket.on('typing', (payload) => {
-        $('#feedback').text(payload.user + ' typing...');
-    })
 });
